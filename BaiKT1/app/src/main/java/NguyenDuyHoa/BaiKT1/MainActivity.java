d3c091ba942c6ddf6b92b45386e1fff2fa16b323
kilongbong <100772403+kilongbong@users.noreply.github.com>
2022-04-11 08:12:42 +0700

    first commit

---
+++ BaiKT1/app/src/main/java/NguyenDuyHoa/BaiKT1/MainActivity.java
@@ -0,0 +1,39 @@
+package NguyenDuyHoa.BaiKT1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtusername;
+    EditText edtpass;
+    TextView KQ;
+    Button btnlg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtusername=(EditText) findViewById(R.id.edtUserName);
+        edtpass=(EditText) findViewById(R.id.edtPass);
+        KQ=(TextView) findViewById(R.id.tv1);
+        btnlg=(Button) findViewById(R.id.btnLG);
+        btnlg.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                String dem = "User and Password is correct" ;
+                String username = "hoa";
+                String password = "hoa";
+                if(edtusername.getText().toString().equals(username) && edtpass.getText().toString().equals(password)){
+                    Toast.makeText(MainActivity.this, "User and Password is correct", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "User and Password is wrong", Toast.LENGTH_SHORT).show();
+                    KQ.setText(String.valueOf(dem));
+                }
+            }
+        });
+    }
+}
